fa1ae80befe3f0270380c4f550b7aaf15cc65af8
malcomowilla <owillamalcom@gmail.com>
2022-11-28 22:32:09 +0300

    completed assighnment

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice (0, 2);
+
+}
+const returnLastTWoDrivers = function (drivers){
+    return drivers.slice(2);
+
+}
+const selectDrivers = [returnFirstTWoDrivers , returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMultiplier){
+    return function (fare){
+        return fare + fareMultiplier
+
+    }
+}
+const fareDoubler = createFareMultiplier (2)
+const fareTripler = createFareMultipler (3)
+
+const selectDifferentDrivers = function (drivers, returnDrivers){
+    return returnDrivers (drivers);
+}
